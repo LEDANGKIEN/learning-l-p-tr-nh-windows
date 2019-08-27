8ec1e94cde166651ecbffb3012633496838bef51
LEDANGKIEN <54473915+LEDANGKIEN@users.noreply.github.com>
2019-08-28 01:47:26 +0700

    làm quen với github
    hoàn thành buôi 1

---
+++ 2001160260_LeDangKien_07dhth5_BTVN_windows/2001160260_LeDangKien_07dhth5_BTVN/Bai1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2001160260_LeDangKien_07dhth5_BTVN
+{
+    public partial class Bai1 : Form
+    {
+        int cleft=1;
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddNewTextbox();
+        }
+        private System.Windows.Forms.TextBox AddNewTextbox()
+        {
+            System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox();
+            this.Controls.Add(txt);
+            txt.Top = cleft * 25;
+            txt.Left = 100;
+            txt.Text = "TextBox" + this.cleft.ToString();
+            cleft = cleft + 1;
+            return txt;
+        }
+    }
+}
